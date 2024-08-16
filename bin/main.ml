352695d93a2060e492bced1b56d5854720f78c32
Michael J Erwin <michaeljameserwin@gmail.com>
2024-08-16 07:46:47 -0700

    FunDB: working MVCC

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,296 @@
-let db = Fun_db.Database.make ()
+[@@@ocaml.warning "-32-27-26"]
 
-let _cx = Fun_db.Connection.make db
+open Fun_db
 
 let () = print_endline "FunDB started"
+
+let exec c cmd args =
+  match Fun_db.Connection.exec_command c cmd args with
+    | Ok((c, r)) -> (c, r)
+    | Error(e) -> (c, e)
+
+let test_read_uncommitted () =
+  print_endline "Read Uncommitted";
+  let db = Fun_db.Database.make () in
+  let db = {db with default_isolation = ReadUncommitted} in
+  let c1 = Fun_db.Connection.make db in
+
+  let (c1, r) = exec c1 "begin" [] in
+  print_endline r;
+  assert (r = "1");
+  print_endline @@ Int.to_string c1.id;
+  print_endline @@ Int.to_string c1.db.next_transaction_id;
+  assert (c1.db.next_transaction_id = 2);
+
+  let c2 = Fun_db.Connection.make c1.db in
+  let (c2, r) = exec c2 "begin" [] in
+
+  let (c1, r) = exec c1 "set" ["a"; "1"] in
+  print_endline r;
+  assert (r = "1");
+
+  (* update is visible to itself *)
+  let (c1, r) = exec c1 "get" ["a"] in
+  print_endline r;
+  assert (r = "1");
+
+  (* update is visible to other readuncommitted tx *)
+  let (c2, r) = exec c2 "get" ["a"] in
+  print_endline r;
+  assert (r = "1");
+
+  let (c1, r) = exec c1 "set" ["a"; "2"] in
+  print_endline r;
+  assert (r = "2");
+
+  let (c1, r) = exec c1 "get" ["a"] in
+  print_endline r;
+  assert (r = "2");
+
+  let (c1, r) = exec c1 "delete" ["a"] in
+  print_endline r;
+  assert (r = "");
+
+  let (c1, r) = exec c1 "get" ["a"] in
+  print_endline r;
+  assert (r = "");
+
+  let (c2, r) = exec c2 "get" ["a"] in
+  print_endline r;
+  assert (r = "");
+  ()
+
+let test_read_committed () =
+  print_endline "Read Committed";
+  let db = Database.make() in
+  let db = {db with default_isolation = ReadCommitted} in
+
+  let c1 = Connection.make db in
+  let (c1, r) = exec c1 "begin" [] in
+
+  let c2 = Connection.make c1.db in
+  let (c2, r) = exec c2 "begin" [] in
+
+  let (c1, r) = exec c1 "set" ["a"; "1"] in
+  print_endline r;
+  assert (r = "1");
+
+  (* update is visible to itself *)
+  let (c1, r) = exec c1 "get" ["a"] in
+  print_endline r;
+  assert (r = "1");
+
+  (* update is not available to others *)
+  let (c2, r) = exec c2 "get" ["a"] in
+  print_endline r;
+  assert (r <> "1");
+
+  let (c1, r) = exec c1 "commit" [] in
+
+  (* update is visible to others after commit *)
+  let (c2, r) = exec c2 "get" ["a"] in
+  print_endline r;
+  assert (r = "1");
+
+  let c3 = Connection.make c2.db in
+  let (c3, r) = exec c3 "begin" [] in
+  let (c3, r) = exec c3 "set" ["b"; "2"] in
+  let (c3, r) = exec c3 "get" ["b"] in
+  print_endline r;
+  assert (r = "2");
+  let (c3, r) = exec c3 "abort" [] in
+
+  let (c2, r) = exec c2 "get" ["b"] in
+  print_endline r;
+  assert (r = "");
+
+  let (c2, r) = exec c2 "delete" ["a"] in
+  let (c2, r) = exec c2 "get" ["a"] in
+  print_endline r;
+  assert (r = "");
+
+  let _ = exec c2 "commit" [] in
+
+  let c4 = Connection.make c3.db in
+  let (c4, _) = exec c4 "begin" [] in
+  let (c4, r) = exec c4 "get" ["a"] in
+  print_endline r;
+  assert (r = "");
+
+  ()
+
+let test_repeatable_read () =
+  print_endline "Repeatable Read";
+  let db = Database.make() in
+  let db = {db with default_isolation = RepeatableRead} in
+
+  let c1 = Connection.make db in
+  let (c1, r) = exec c1 "begin" [] in
+
+  let c2 = Connection.make c1.db in
+  let (c2, r) = exec c2 "begin" [] in
+
+  let (c1, r) = exec c1 "set" ["a"; "1"] in
+  print_endline r;
+  assert (r = "1");
+
+  (* update is visible to itself *)
+  let (c1, r) = exec c1 "get" ["a"] in
+  print_endline r;
+  assert (r = "1");
+
+  (* update is not available to others *)
+  let (c2, r) = exec c2 "get" ["a"] in
+  print_endline r;
+  assert (r <> "1");
+
+  let (c1, r) = exec c1 "commit" [] in
+
+  (* update is not available to existing tx even after commit *)
+  let (c2, r) = exec c2 "get" ["a"] in
+  let _ = print_endline ("c2 get after commit of c1 " ^ r) in
+  assert (r <> "1");
+
+  (* update is visible to new transactions after commit*)
+  let c3 = Connection.make c2.db in
+
+  let (c3, r) = exec c3 "begin" [] in
+  let (c3, r) = exec c3 "get" ["a"] in
+  print_endline r;
+  assert (r = "1");
+
+  (* update is visible to itself *)
+  let (c3, r) = exec c3 "set" ["b"; "2"] in
+  let (c3, r) = exec c3 "get" ["b"] in
+  print_endline r;
+  assert (r = "2");
+  let (c3, r) = exec c3 "abort" [] in
+
+  (* update isn't visible to transactions after abort *)
+  let (c2, r) = exec c2 "get" ["b"] in
+  print_endline r;
+  assert (r = "");
+
+  (* delete is visible to itself *)
+  let c4 = Connection.make c3.db in
+  let (c4, _) = exec c4 "begin" [] in
+  let (c4, r) = exec c4 "get" ["a"] in
+  print_endline r;
+  assert (r = "1");
+  let (c4, r) = exec c4 "delete" ["a"] in
+  print_endline r;
+  assert (r = "");
+  let (c4, r) = exec c4 "get" ["a"] in
+  print_endline r;
+  assert (r = "");
+
+  let _ = exec c4 "commit" [] in
+
+  (* delete is visible to new transaction after commit *)
+  let c5 = Connection.make c4.db in
+  let (c5, _) = exec c5 "begin" [] in
+  let (c5, r) = exec c5 "get" ["a"] in
+  print_endline r;
+  assert (r = "");
+
+  ()
+
+let test_snapshot_isolation_write_write_conflict () =
+  print_endline "Snapshot";
+
+  let db = Database.make() in
+
+  let db = {db with default_isolation = Snapshot} in
+
+  let c1 = Connection.make db in
+  let (c1, r) = exec c1 "begin" [] in
+
+  let c2 = Connection.make c1.db in
+  let (c2, r) = exec c2 "begin" [] in
+
+  let (c1, r) = exec c1 "set" ["a"; "1"] in
+  print_endline r;
+  assert (r = "1");
+  let _ = exec c1 "commit" [] in
+
+  let (c2, r) = exec c2 "set" ["a"; "2"] in
+  print_endline r;
+  let _ = exec c2 "commit" [] in
+
+  let c3 = Connection.make c2.db in
+  let (c3, r) = exec c3 "begin" [] in
+  let (c3, r) = exec c3 "get" ["a"] in
+  print_endline r;
+  assert (r = "1");
+
+  let (c3, r) = exec c3 "set" ["b"; "2"] in
+  print_endline r;
+  assert (r = "2");
+  let _ = exec c3 "commit" [] in
+
+  let tx3 = Hashtbl.find db.transactions 3 in
+  assert (tx3.state = Committed);
+
+  let tx2 = Hashtbl.find db.transactions 2 in
+  assert (tx2.state = Aborted);
+
+  let tx1 = Hashtbl.find db.transactions 1 in
+  assert (tx1.state = Committed);
+
+  let v = match Hashtbl.find db.store "b" with | [v] -> Some v | _ -> None in
+  let v = Option.get v in
+  assert (v.value = "2");
+
+  ()
+
+let test_serializable_isolation_readwrite_conflict () =
+  print_endline "Serializable";
+
+  let db = Database.make() in
+
+  let db = {db with default_isolation = Serializable} in
+
+  let c1 = Connection.make db in
+  let (c1, r) = exec c1 "begin" [] in
+
+  let c2 = Connection.make c1.db in
+  let (c2, r) = exec c2 "begin" [] in
+
+  let c3 = Connection.make c2.db in
+  let (c3, r) = exec c3 "begin" [] in
+
+  let (c1, r) = exec c1 "set" ["a"; "1"] in
+  print_endline r;
+  assert (r = "1");
+  let _ = exec c1 "commit" [] in
+
+  let (c2, r) = exec c2 "get" ["a"] in
+  print_endline r;
+  assert (r = "");
+  let _ = exec c2 "commit" [] in
+
+  let (c3, r) = exec c3 "set" ["b"; "2"] in
+  print_endline r;
+  assert (r = "2");
+  let _ = exec c3 "commit" [] in
+
+  let tx3 = Hashtbl.find db.transactions 3 in
+  assert (tx3.state = Committed);
+
+  let tx2 = Hashtbl.find db.transactions 2 in
+  assert (tx2.state = Aborted);
+
+  let tx1 = Hashtbl.find db.transactions 1 in
+  assert (tx1.state = Committed);
+
+  let v = match Hashtbl.find db.store "b" with | [v] -> Some v | _ -> None in
+  let v = Option.get v in
+  assert (v.value = "2");
+
+  ()
+
+let () = test_read_uncommitted ()
+let () = test_read_committed ()
+let () = test_repeatable_read ()
+let () = test_snapshot_isolation_write_write_conflict ()
+let () = test_serializable_isolation_readwrite_conflict ()
--- lib/fun_db.ml
+++ lib/fun_db.ml
@@ -15,8 +15,6 @@ let _debug a =
   let args = ["hey"; "you"; "guys"; a] in
   print_endline @@ List.fold_right (^) args ""
 
-let () = print_endline "Hello, World!"
-
 type value = {
     tx_start_id : int;
     tx_end_id : int;
@@ -26,7 +24,7 @@ type value = {
 type transaction_state = InProgress | Aborted | Committed
 
 type isolation_level =
-  ReadUncommited
+  ReadUncommitted
 | ReadCommitted
 | RepeatableRead
 | Snapshot
@@ -42,11 +40,9 @@ type transaction = {
     (* Used only by Repeatable Read and stricter. *)
     inprogress: IntSet.t;
    (*  Used only by Snapshot Isolation and stricter. *)
-    writeset: StringSet.t option;
-    readset: StringSet.t option;
+    writeset: StringSet.t;
+    readset: StringSet.t;
   }
-
-
 
 module Database =
   struct
@@ -76,21 +72,57 @@ module Database =
           state = InProgress;
           id = db.next_transaction_id;
           inprogress = in_progress db;
-          writeset = None;
-          readset = None;
+          writeset = StringSet.empty;
+          readset = StringSet.empty;
         } in
       let db = {db with next_transaction_id = db.next_transaction_id + 1} in
       Hashtbl.add db.transactions tx.id tx;
       (db, tx)
 
-    let complete_transaction db tx state =
+    let txs_after db tx_id =
+      let tx_ids = Hashtbl.to_seq_keys db.transactions in
+      let tx_ids = List.to_seq @@ List.sort Int.compare @@ List.of_seq tx_ids in
+      let tx_ids = List.of_seq @@ Seq.drop_while (( >= ) tx_id) tx_ids in
+      tx_ids
+
+    let has_conflict (db: t) (tx: transaction) (p: transaction -> transaction -> bool) =
+      let test = fun tx2_id ->
+        let tx2 = Hashtbl.find db.transactions tx2_id in
+        tx2.state = Committed && p tx tx2 in
+
+      let ip_conflict = List.find_opt test (IntSet.to_list tx.inprogress) in
+      let post_conflict = List.find_opt test (txs_after db tx.id) in
+      Option.is_some ip_conflict || Option.is_some post_conflict
+
+    let first_item s1 =
+      match StringSet.to_list s1 with [] -> "" | v :: _ -> v
+
+    let sets_share_item s1 s2 = not (StringSet.disjoint s1 s2)
+
+    (* TODO: this should return a result *)
+    let rec complete_transaction db tx state =
       (* debug("completing transaction ", t.id) *)
-      let tx = {tx with state = state} in
+      if state = Committed
+         && ((tx.isolation = Snapshot
+              && has_conflict db tx (fun tx1 tx2 -> sets_share_item tx1.writeset tx2.writeset))
+             || (tx.isolation = Serializable
+                 && has_conflict db tx (fun tx1 tx2 -> sets_share_item tx1.readset tx2.writeset
+                                                       || sets_share_item tx1.writeset tx2.readset)))
+
+                         (* Error "write-write conflict" *)
+      then let _ = print_endline @@ "complete tx failed for id " ^ Int.to_string tx.id in
+           complete_transaction db tx Aborted
+      else let tx = {tx with state = state} in
       Hashtbl.replace db.transactions tx.id tx
 
+    (* should just change name to get_transaction *)
     let transaction_state db tx_id =
       let opt_tx = Hashtbl.find_opt db.transactions tx_id in
       Option.to_result ~none:"invalid transaction" opt_tx
+
+    let update_transaction db tx =
+      Hashtbl.replace db.transactions tx.id tx;
+      tx
 
     let assert_valid_transaction db tx_opt =
       let* tx = Option.to_result ~none:"transaction is None" tx_opt in
@@ -100,19 +132,54 @@ module Database =
       | InProgress -> Ok tx
       | _ -> Error "transaction not in progress"
 
-    let is_visible _v = true
+    let is_read_committed_visible db tx v =
+      let start_tx_c = transaction_state db v.tx_start_id
+                       |> Result.map (fun tx -> tx.state = Committed)
+                       |> Result.get_ok in
+      let end_tx_c_result = transaction_state db v.tx_end_id
+                            |> Result.map (fun tx -> tx.state = Committed) in
+      not (v.tx_start_id <> tx.id && not start_tx_c)
+      && not (v.tx_end_id = tx.id)
+      && not (v.tx_end_id > 0 && Result.get_ok end_tx_c_result)
+
+    (* TODO refactor this to use not && *)
+    let is_repeatable_read_visible db tx v =
+      if v.tx_start_id > tx.id
+      then false
+      else if Option.is_some (IntSet.find_opt v.tx_start_id tx.inprogress)
+      then false
+      else if v.tx_start_id <> tx.id
+              && (Result.get_ok (transaction_state db v.tx_start_id)).state <> Committed
+      then false
+      else if v.tx_end_id = tx.id
+      then false
+      else if v.tx_end_id < tx.id
+              && v.tx_end_id > 0
+              && (Result.get_ok (transaction_state db v.tx_end_id)).state = Committed
+              && Option.is_none (IntSet.find_opt v.tx_end_id tx.inprogress)
+      then false
+      else true
+
+    let is_visible db tx v =
+      match tx.isolation with
+      | ReadUncommitted -> v.tx_end_id = 0
+      | ReadCommitted -> is_read_committed_visible db tx v
+      | _ -> is_repeatable_read_visible db tx v
+
 
   end
 
 module Connection =
   struct
     type t = {
+        id : int;
         tx : transaction option;
         db : Database.t;
       }
 
-    let make db = {
+    let make (db: Database.t) = {
         db;
+        id = db.next_transaction_id;
         tx = None;
       }
 
@@ -120,12 +187,12 @@ module Connection =
       let* _ = assert_eq c.tx None "no running transactions" in
       let (db, tx) = Database.new_transaction c.db in
       let* tx = Database.assert_valid_transaction db (Some tx) in
-      Ok(({db; tx = Some tx}, Int.to_string tx.id))
+      Ok(({c with db = db; tx = Some tx}, Int.to_string tx.id))
 
     let abort_tx c =
       let* tx = Database.assert_valid_transaction c.db c.tx in
       Database.complete_transaction c.db tx Aborted;
-      Ok({c with tx = Some tx}, "")
+      Ok({c with tx = None}, "")
 
     let commit_tx c =
       let* tx = Database.assert_valid_transaction c.db c.tx in
@@ -133,38 +200,49 @@ module Connection =
       Ok({c with tx = None}, "")
 
     let get c args =
+      print_endline @@ "get on connection " ^ Int.to_string c.id;
       let* tx = Database.assert_valid_transaction c.db c.tx in
       let* key = Option.to_result ~none:"no key provided" (List.nth_opt args 0) in
-      let readset = StringSet.add key (Option.get tx.readset) in
+      let readset = StringSet.add key tx.readset in
+      let tx = Database.update_transaction c.db {tx with readset = readset} in
       let vs = Hashtbl.find_opt c.db.store key in
       let* vs = Option.to_result ~none:"no value for key exists" vs in
       (* debug(value, c.tx, c.db.isvisible(c.tx, value)) *)
-      let v = List.find_opt Database.is_visible vs in
+      let v = List.find_opt (Database.is_visible c.db tx) vs in
       let v = Option.map (fun v -> v.value) v in
       let v = Option.value ~default:"" v in
-      (* TODO: replace transaction in db.transactions *)
-      Ok({c with tx = Some({tx with readset = Some(readset)})}, v)
+      Ok({c with tx = Some(tx)}, v)
 
-    let invalidate_versions tx_id vs =
-      List.fold_left (fun (l, f) v -> if Database.is_visible v
-                                      then ({v with tx_end_id = tx_id} :: l, true)
-                                      else (v :: l, f))
-        ([], false)
+    let invalidate_versions db tx vs =
+      List.fold_left_map (fun f v -> if Database.is_visible db tx v
+                                      then (true, {v with tx_end_id = tx.id})
+                                      else (f, v))
+        false
         vs
 
     let set c args =
       let* tx = Database.assert_valid_transaction c.db c.tx in
       let* key = Option.to_result ~none:"no key provided" (List.nth_opt args 0) in
-      let vs = Hashtbl.find_opt c.db.store key in
-      let* vs = Option.to_result ~none:"no value for key exists" vs in
-      let (vs, _) = invalidate_versions tx.id vs in
-      let writeset = StringSet.add key (Option.get tx.writeset) in
+      let vs = Option.value ~default:[] @@ Hashtbl.find_opt c.db.store key in
+      let (_, vs) = invalidate_versions c.db tx vs in
+      let writeset = StringSet.add key tx.writeset in
+      let tx = Database.update_transaction c.db {tx with writeset = writeset} in
       let* value = Option.to_result ~none:"no val provided" (List.nth_opt args 1) in
       let value = {tx_start_id = tx.id; tx_end_id = 0; value} in
       Hashtbl.replace c.db.store key (value :: vs);
-      (* TODO: replace transaction in db.transactions *)
-      Ok({c with tx = Some({tx with writeset = Some(writeset)})}, value.value)
+      Ok({c with tx = Some(tx)}, value.value)
 
+    let delete c args =
+      let* tx = Database.assert_valid_transaction c.db c.tx in
+      let* key = Option.to_result ~none:"no key provided" (List.nth_opt args 0) in
+      let vs = Hashtbl.find_opt c.db.store key in
+      let* vs = Option.to_result ~none:"no value for key exists" vs in
+      let (found, vs) = invalidate_versions c.db tx vs in
+      let* _ = if found then Ok(()) else Error("cannot delete key that does not exist") in
+      let writeset = StringSet.add key tx.writeset in
+      let tx = Database.update_transaction c.db {tx with writeset = writeset} in
+      Hashtbl.replace c.db.store key vs;
+      Ok({c with tx = Some(tx)}, "")
 
     let exec_command c command args =
       (* debug(command, args) *)
@@ -174,6 +252,7 @@ module Connection =
       | "commit" -> commit_tx c
       | "get" -> get c args
       | "set" -> set c args
+      | "delete" -> delete c args
       | _ -> Error "unimplemented"
 
     let must_exec_command c command args =
